eafcb7f8d829d9a94d24efdad921ce73617067f1
jmuller122905 <julien.muller@sgforge.com>
2024-09-06 11:57:27 +0200

    more explicit error message in case of invalid ETH address

--- src/v1/scripts/configuration/validations.ts
+++ src/v1/scripts/configuration/validations.ts
@@ -2,5 +2,5 @@ import { ethers } from 'ethers';
 
 export const IsETHAddress = [
   (val: string) => ethers.isAddress(val),
-  { message: 'Must be a valid ETH address ( checksum must be valid )' },
+  { message: 'Must be a valid ETH address ( you can use etherscan to get the right casing if necessary)' },
 ] as const;
